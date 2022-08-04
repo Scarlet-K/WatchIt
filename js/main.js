2f6d20ef7947a3adae6fcf4485c061ffb275f8bf
Scarlet Kim <scarletkim714@gmail.com>
2022-08-04 04:44:42 +0000

    created tabs for categories and view swapping for each tab. #1

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-function getNowPlayingImg() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://api.themoviedb.org/3/movie/now_playing?api_key=d7a558bf3c164e7e0d8761462a9973e2');
-  xhr.responseType = 'json';
-  xhr.addEventListener('load', function () {
-    for (var i = 0; i < 8; i++) {
-      images.push('https://image.tmdb.org/t/p/w500/' + xhr.response.results[i].poster_path);
-      $carouselImg.setAttribute('src', images[0]);
-    }
-  });
-  xhr.send();
-}
-
-getNowPlayingImg();
-
 var images = [];
 var $carouselImg = document.querySelector('.carousel-image');
 var intervalID = setInterval(getNextIndex, 3000);
@@ -94,3 +79,70 @@ function setIndex() {
   clearInterval(intervalID);
   intervalID = setInterval(getNextIndex, 3000);
 }
+
+function getCarouselImg() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.themoviedb.org/3/movie/now_playing?api_key=d7a558bf3c164e7e0d8761462a9973e2');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    for (var i = 0; i < 8; i++) {
+      images.push('https://image.tmdb.org/t/p/w500/' + xhr.response.results[i].poster_path);
+      $carouselImg.setAttribute('src', images[0]);
+    }
+  });
+  xhr.send();
+}
+
+getCarouselImg();
+
+// change category view //
+var $tabContainer = document.querySelector('.tab-container');
+var $tabList = document.querySelectorAll('.tab');
+var $viewList = document.querySelectorAll('.view');
+
+$tabContainer.addEventListener('click', handleTabClick);
+
+function handleTabClick(event) {
+  if (!event.target.matches('.tab')) {
+    return;
+  }
+
+  for (var i = 0; i < $tabList.length; i++) {
+    if (event.target === $tabList[i]) {
+      $tabList[i].classList.add('active');
+    } else {
+      $tabList[i].classList.remove('active');
+    }
+  }
+
+  var $eventView = event.target.getAttribute('data-view');
+
+  for (var k = 0; k < $viewList.length; k++) {
+    if ($eventView === $tabList[k].getAttribute('data-view')) {
+      $viewList[k].classList.remove('hidden');
+    } else {
+      $viewList[k].classList.add('hidden');
+    }
+  }
+}
+
+// var $topRated = document.querySelector('.top-rated');
+// console.log($topRated);
+
+function getCategoryImg() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.themoviedb.org/3/movie/top_rated?api_key=d7a558bf3c164e7e0d8761462a9973e2');
+  xhr.responseType = 'json';
+  // console.log(xhr.status);
+  // console.log(xhr.response);
+  xhr.addEventListener('load', function () {
+    // for (var i = 0; i < xhr.response.results; i++) {
+    //   var $categoryImg = document.createElement('img');
+    //   $categoryImg.setAttribute('src', 'https://image.tmdb.org/t/p/w500/' + xhr.response.results[i].poster_path);
+    //   $topRated.appendChild($categoryImg);
+    // }
+  });
+  xhr.send();
+}
+
+getCategoryImg();
