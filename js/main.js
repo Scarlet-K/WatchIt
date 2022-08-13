31ea260d57d3cef7969980fb6ab01e6e72ab0c30
Scarlet Kim <scarletkim714@gmail.com>
2022-08-13 01:01:55 +0000

    In progress: rendering detail page. #5

--- js/main.js
+++ js/main.js
@@ -192,15 +192,15 @@ function handleNav(event) {
   viewSwap(event.target.getAttribute('data-view'));
 }
 
-function addMovie(event) {
-  if (event.target.tagName === 'BUTTON') {
-    for (var i = 0; i < data.movies.length; i++) {
-      if (Number.parseInt(event.target.id) === data.movies[i].id) {
-        data.watchlist.unshift(data.movies[i]);
-      }
-    }
-  }
-}
+// function addMovie(event) {
+//   if (event.target.tagName === 'BUTTON') {
+//     for (var i = 0; i < data.movies.length; i++) {
+//       if (Number.parseInt(event.target.id) === data.movies[i].id) {
+//         data.watchlist.unshift(data.movies[i]);
+//       }
+//     }
+//   }
+// }
 
 function renderMyList(watchlist) {
   /*
@@ -230,9 +230,9 @@ function showDetails(event) {
   }
   if (event.target.tagName === ('IMG')) {
     var targetId = event.target.id;
-    while ($detail.firstChild) {
-      $detail.removeChild($detail.firstChild);
-    }
+    // while ($detail.firstChild) {
+    //   $detail.removeChild($detail.firstChild);
+    // }
     getDetails(targetId);
     $detail.classList.remove('hidden');
     $home.classList.add('hidden');
@@ -245,7 +245,7 @@ function getDetails(id) {
   xhr.open('GET', 'https://api.themoviedb.org/3/movie/' + id + '?api_key=d7a558bf3c164e7e0d8761462a9973e2&language=en-US');
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
-    renderDetails(xhr.response);
+    // renderDetails(xhr.response);
     var currentMovie = {
       id: xhr.response.id,
       poster_path: 'https://image.tmdb.org/t/p/w500/' + xhr.response.poster_path
@@ -255,100 +255,123 @@ function getDetails(id) {
   xhr.send();
 }
 
-function renderDetails(response) {
-/*
-<div class= "row pd-tb3">
-  <div class= "col-half">
-    <div class= "row">
-      <div class= "col-full pd-lr detail-poster">
-        <img>
-      </div>
-      <div class= "row text-end pd-tb1">
-        <div class= "col-full pd-lr">
-          <button class= "add-button">Add to My List</button>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class= "col-half pd-lr font-ver detail-info">
-    <h2></h2>
-    <p class= "font-work grey-font"></p>
-    <div class= "pd-tb">
-      <h4></h4>
-      <p class= "font-work font-s grey-font"></p>
-      <h4></h4>
-      <span class= "font-work font-s grey-font"></span>
-      <h4></h4>
-      <span class= "font-work font-s grey-font"></span>
-    </div>
-  </div>
-</div>
-*/
-  var $row = document.createElement('div');
-  $row.setAttribute('class', 'row pd-tb3');
-  var $colHalf = document.createElement('div');
-  $colHalf.setAttribute('class', 'col-half');
-  var $posterRow = document.createElement('div');
-  $posterRow.setAttribute('class', 'row');
-  var $detailPoster = document.createElement('div');
-  $detailPoster.setAttribute('class', 'col-full pd-lr detail-poster');
-  var $img = document.createElement('img');
-  $img.setAttribute('src', 'https://image.tmdb.org/t/p/w500/' + response.poster_path);
-  var $rowButton = document.createElement('div');
-  $rowButton.setAttribute('class', 'row text-end pd-tb1');
-  var $colFull = document.createElement('div');
-  $colFull.setAttribute('class', 'col-full pd-lr');
-  var $button = document.createElement('button');
-  $button.setAttribute('class', 'add-button');
-  $button.setAttribute('id', response.id);
-  $button.textContent = 'Add to My List';
-  $button.addEventListener('click', addMovie);
-  var $detailInfo = document.createElement('div');
-  $detailInfo.setAttribute('class', 'col-half pd-lr font-ver detail-info');
-  var $h2 = document.createElement('h2');
-  $h2.textContent = response.title;
-  var $star = document.createElement('i');
-  $star.setAttribute('class', 'fa-solid fa-star yellow');
-  var $rating = document.createElement('span');
-  $rating.setAttribute('class', 'pd-lr-h');
-  $rating.textContent = response.vote_average.toFixed(1);
-  var $p = document.createElement('p');
-  $p.textContent = response.overview;
-  $p.setAttribute('class', 'font-work grey-font');
-  var $div = document.createElement('div');
-  $div.setAttribute('class', 'pd-tb');
-  var $date = document.createElement('h4');
-  $date.textContent = 'Release Date';
-  var $prod = document.createElement('h4');
-  $prod.textContent = 'Production Companies';
-  var $genre = document.createElement('h4');
-  $genre.textContent = 'Genres';
-  var $dateP = document.createElement('p');
-  $dateP.textContent = response.release_date;
-  $dateP.setAttribute('class', 'font-work font-s grey-font');
-  $div.append($date, $prod, $genre);
-  for (var i = 0; i < response.production_companies.length; i++) {
-    var $prodP = document.createElement('p');
-    $prodP.textContent = response.production_companies[i].name;
-    $prodP.setAttribute('class', 'font-work font-s grey-font');
-    $prod.after($prodP);
-  }
-  for (var k = 0; k < response.genres.length; k++) {
-    var $genreP = document.createElement('p');
-    $genreP.textContent = response.genres[k].name;
-    $genreP.setAttribute('class', 'font-work font-s grey-font');
-    $genre.after($genreP);
-  }
-  $row.appendChild($colHalf);
-  $colHalf.appendChild($posterRow);
-  $posterRow.appendChild($detailPoster);
-  $detailPoster.appendChild($img);
-  $colHalf.appendChild($rowButton);
-  $colFull.append($button);
-  $rowButton.appendChild($colFull);
-  $row.appendChild($detailInfo);
-  $detailInfo.append($h2, $star, $rating, $p);
-  $detailInfo.appendChild($div);
-  $date.after($dateP);
-  $detail.appendChild($row);
-}
+// function renderDetails() {
+
+// }
+
+// function renderDetails(response) {
+// /*
+// <div class= "row pd-tb3">
+//   <div class= "col-half">
+//     <div class= "row">
+//       <div class= "col-full pd-lr detail-poster">
+//         <img>
+//       </div>
+//       <div class= "row text-end pd-tb1">
+//         <div class= "col-full pd-lr">
+//           <button class= "add-button">Add to My List</button>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+//   <div class= "col-half pd-lr font-ver detail-info">
+//     <h2></h2>
+//     <i class= "fa-solid fa-star yellow"></i>
+//     <span class= "pd-lr-h"></span>
+//     <p class= "font-work grey-font"></p>
+//     <div class= "pd-tb">
+//       <h4></h4>
+//       <p class= "font-work font-s grey-font"></p>
+//       <h4></h4>
+//       <span class= "font-work font-s grey-font"></span>
+//       <h4></h4>
+//       <span class= "font-work font-s grey-font"></span>
+//     </div>
+//   </div>
+// </div>
+// */
+//   var $row = document.createElement('div');
+//   $row.setAttribute('class', 'row pd-tb3');
+//   var $colHalf = document.createElement('div');
+//   $colHalf.setAttribute('class', 'col-half');
+//   var $posterRow = document.createElement('div');
+//   $posterRow.setAttribute('class', 'row');
+//   var $detailPoster = document.createElement('div');
+//   $detailPoster.setAttribute('class', 'col-full pd-lr detail-poster');
+//   var $img = document.createElement('img');
+//   $img.setAttribute('src', 'https://image.tmdb.org/t/p/w500/' + response.poster_path);
+//   var $rowButton = document.createElement('div');
+//   $rowButton.setAttribute('class', 'row text-end pd-tb1');
+//   var $colFull = document.createElement('div');
+//   $colFull.setAttribute('class', 'col-full pd-lr');
+//   var $button = document.createElement('button');
+//   $button.setAttribute('class', 'add-button');
+//   $button.setAttribute('id', response.id);
+//   $button.textContent = 'Add to My List';
+//   $button.addEventListener('click', addMovie);
+//   // console.log(data.watchlist);
+//   // console.log(response.id);
+//   for (var t = 0; t < data.watchlist.length; t++) {
+//     for (var y = 0; y < data.movies.length; y++) {
+//       if (data.movies[y].id === data.watchlist[t].id) {
+//         $button.setAttribute('class', 'remove-button');
+//         $button.setAttribute('id', response.id);
+//         $button.textContent = 'Remove';
+//         // $button.addEventListener('click', addMovie);
+//       } else {
+//         $button.setAttribute('class', 'add-button');
+//         $button.setAttribute('id', response.id);
+//         $button.textContent = 'Add to My List';
+//         $button.addEventListener('click', addMovie);
+//       }
+//     }
+//   }
+//   var $detailInfo = document.createElement('div');
+//   $detailInfo.setAttribute('class', 'col-half pd-lr font-ver detail-info');
+//   var $h2 = document.createElement('h2');
+//   $h2.textContent = response.title;
+//   var $star = document.createElement('i');
+//   $star.setAttribute('class', 'fa-solid fa-star yellow');
+//   var $rating = document.createElement('span');
+//   $rating.setAttribute('class', 'pd-lr-h');
+//   $rating.textContent = response.vote_average.toFixed(1);
+//   var $p = document.createElement('p');
+//   $p.textContent = response.overview;
+//   $p.setAttribute('class', 'font-work grey-font');
+//   var $div = document.createElement('div');
+//   $div.setAttribute('class', 'pd-tb');
+//   var $date = document.createElement('h4');
+//   $date.textContent = 'Release Date';
+//   var $prod = document.createElement('h4');
+//   $prod.textContent = 'Production Companies';
+//   var $genre = document.createElement('h4');
+//   $genre.textContent = 'Genres';
+//   var $dateP = document.createElement('p');
+//   $dateP.textContent = response.release_date;
+//   $dateP.setAttribute('class', 'font-work font-s grey-font');
+//   $div.append($date, $prod, $genre);
+//   for (var i = 0; i < response.production_companies.length; i++) {
+//     var $prodP = document.createElement('p');
+//     $prodP.textContent = response.production_companies[i].name;
+//     $prodP.setAttribute('class', 'font-work font-s grey-font');
+//     $prod.after($prodP);
+//   }
+//   for (var k = 0; k < response.genres.length; k++) {
+//     var $genreP = document.createElement('p');
+//     $genreP.textContent = response.genres[k].name;
+//     $genreP.setAttribute('class', 'font-work font-s grey-font');
+//     $genre.after($genreP);
+//   }
+//   $row.appendChild($colHalf);
+//   $colHalf.appendChild($posterRow);
+//   $posterRow.appendChild($detailPoster);
+//   $detailPoster.appendChild($img);
+//   $colHalf.appendChild($rowButton);
+//   $colFull.append($button);
+//   $rowButton.appendChild($colFull);
+//   $row.appendChild($detailInfo);
+//   $detailInfo.append($h2, $star, $rating, $p);
+//   $detailInfo.appendChild($div);
+//   $date.after($dateP);
+//   $detail.appendChild($row);
+// }
